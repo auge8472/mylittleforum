03b7eb5475af4d316bf949c5928b73ea9da4a176
Heiko August <post@auge8472.de>
2022-04-24 20:19:24 +0200

    Refactor: function is_pw_correct

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1852,28 +1852,6 @@ function create_backup_file($mode = 0) {
 }
 
 /**
- * checks password comparing it with the hash
- *
- * @param string $pw
- * @param string $hash
- * @return bool
- */
-function is_pw_correct($pw,$hash)
- {
-  if(strlen($hash)==50) // salted sha1 hash with salt
-   {
-    $salted_hash = substr($hash,0,40);
-    $salt = substr($hash,40,10);
-    if(sha1($pw.$salt)==$salted_hash) return true;
-    else return false;
-   }
-  elseif(strlen($hash)==32) // md5 hash generated in an older version
-   {
-    if($hash == md5($pw)) return true;
-    else return false;
-   }
-  else return false;
- }
 
 /**
  * add "http://" to url if given without protocol
@@ -1922,6 +1900,26 @@ function restore_backup($backup_file) {
 }
 
 /**
+ * checks password comparing it with the hash
+ *
+ * @param string $pw
+ * @param string $hash
+ * @return bool
+ */
+function is_pw_correct($pw,$hash) {
+	if (strlen($hash) == 50) {
+		// salted sha1 hash with salt
+		$salted_hash = substr($hash, 0, 40);
+		$salt = substr($hash, 40, 10);
+		if (sha1($pw.$salt) == $salted_hash) return true;
+		else return false;
+	} elseif (strlen($hash) == 32) {
+		// md5 hash generated in an older version
+		if($hash == md5($pw)) return true;
+		else return false;
+	}
+	else return false;
+}
  * determine string length using mb_strlen if available or strlen if not
  *
  * @param string $string
