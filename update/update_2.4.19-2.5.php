199ed52cfb9ee5f166c827f71d5dae60bcf43af5
Heiko August <post@auge8472.de>
2026-04-28 22:23:23 +0200

    Change: copy the structure and data of the smilies table and perform the structural changes

--- update/update_2.4.19-2.5.php
+++ update/update_2.4.19-2.5.php
@@ -301,6 +301,49 @@ if (empty($update['errors']) && in_array($settings['version'], array('2.4.19', '
 			if (!@mysqli_query($connid, "DROP TABLE IF EXISTS `" . $db_settings['uploads_table'] . "`")) $update['errors'][] = 'Database error in line '.__LINE__.': ' . mysqli_error($connid);
 			
 			
+			// changes in the smilies table
+			$statusTestSmiliesTable = true;
+			if (empty($update['errors'])) {
+				$qCopyTable = "CREATE TABLE IF NOT EXISTS `". $db_settings['smilies_table'] ."_tmp` 
+					LIKE `". $db_settings['smilies_table'] ."`;";
+				if (!mysqli_query($connid, $qCopyTable) {
+					$update['errors'] = 'Database error in line '. (__LINE__ - 1) .': ' . mysqli_error($connid);
+					$statusTestSmiliesTable = false;
+				} else {
+					$update['status'] = 'Smilies table copied.';
+				}
+			}
+			if (empty($update['errors'])) {
+				$qCopyData = "INSERT `". $db_settings['smilies_table'] ."_tmp`
+					SELECT * FROM `". $db_settings['smilies_table'] ."`;";
+				if (!mysqli_query($connid, $qCopyData)) {
+					$update['errors'] = 'Database error in line '. (__LINE__ - 1) .': ' . mysqli_error($connid);
+					$statusTestSmiliesTable = false;
+				} else {
+					$update['status'] = 'Data of smilies table copied.';
+				}
+			}
+			if (empty($update['errors'])) {
+				$qAlterTable = "ALTER TABLE `". $db_settings['smilies_table'] ."_tmp`
+					CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
+				if (!mysqli_query($connid, $qAlterTable)) {
+					$update['errors'] = 'Database error in line '. (__LINE__ - 1) .': ' . mysqli_error($connid);
+					$statusTestSmiliesTable = false;
+				} else {
+					$update['status'] = 'Structure of smilies table altered.';
+				}
+			}
+			if (empty($update['errors'])) {
+				$qAlterTable = "ALTER TABLE `". $db_settings['smilies_table'] ."_tmp`
+					CHANGE `id` `id` int UNSIGNED NOT NULL AUTO_INCREMENT";
+				if (!mysqli_query($connid, $qAlterTable)) {
+					$update['errors'] = 'Database error in line '. (__LINE__ - 1) .': ' . mysqli_error($connid);
+					$statusTestSmiliesTable = false;
+				} else {
+					$update['status'] = 'Structure of columns in smilies table altered.';
+				}
+			}
+			
 			// changes in the subscriptions table
 			$statusTestSubscriptionsTable = true;
 			if (empty($update['errors'])) {
