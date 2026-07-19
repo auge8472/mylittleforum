629b5b392656c36fbd2d6d3e249e64a21fe5f316
Heiko August <post@auge8472.de>
2026-07-19 12:06:57 +0200

    Cleanup: correct the phrasing and a typo in comments

--- js/main.js
+++ js/main.js
@@ -1210,7 +1210,7 @@ function getCapsLock(thisEl) {
 		};
 		
 		/**
-		 * Add a checkbox to a INPUT element of type PASSWORD to show/hid the entered password
+		 * Add a checkbox to an INPUT element of type PASSWORD to show/hide the entered password
 		 */
 		var togglePasswordVisibility = function() {
 			if (document.getElementById("content")) {
@@ -1513,8 +1513,8 @@ function getCapsLock(thisEl) {
 			mlf.init(ajaxPreviewStructure);
 		new DragAndDropTable(document.getElementById("sortable"), "bookmarks", "mode", "admin", "action");
 		// search for password input fields to monitor whether the Caps Lock key is pressed or not
-		// do NOT use the input type 'hidden' for searching because we change it for looking at the passwords to 'text'
 		const fldPassWords = document.querySelectorAll('#reg_pw, #new-pw, #old-pw, #pw_new_email, #password, #id_update_password, #confirm_pw_uninstall, #confirm_pw_reset, #ar_pw');
+		// do NOT use the input type 'password' for searching because we change it for looking at the passwords to 'text'
 		if (fldPassWords.length > 0)
 			fldPassWords.forEach(fldPassWords => fldPassWords.addEventListener("keyup", getCapsLock.bind(this)));
 	});
